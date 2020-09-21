6f9e90dc938cc299a042085f87c1f50e414ddfd3
KARPOVTOP <coolxex86@gmail.com>
2020-09-21 20:05:41 +0300

    Initial commit

---
+++ src/Author/TestAuthor.java
@@ -0,0 +1,8 @@
+package Author;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author b1 = new Author("Ivan","Ivan@mail.ru",'M');
+        System.out.println(b1);
+    }
+}
